88ad57a8e8fed0aae047fdcdfccaa19c8a98ab07
Jascha <jascha@jaeh.at>
2019-01-07 03:22:20 +0100

    add example dir link, format

--- example/pages/index.js
+++ example/pages/index.js
@@ -1,16 +1,28 @@
 module.exports = {
-  Body: () => div([
-    h1('@magic-modules/pre'),
-    p('this is the Pre component'),
-    h3('Usage:'),
-    p('first add the component to the assets'),
-    Pre.View(`
+  Body: () =>
+    div([
+      h1('@magic-modules/pre'),
+      p('this is the Pre component'),
+      h3('Usage:'),
+      p('first add the component to the assets'),
+      Pre.View(`
 // assets/index.js
 module.exports = {
   //...other exports
   Pre: require('@magic-modules/pre'),
 }`),
-    p('then use it somewhere in a page/component'),
-    Pre.View("module.exports = {\n  View: () => Pre.View('const js = true'),\n}"),
-  ]),
+      p('then use it somewhere in a page/component'),
+      Pre.View("module.exports = {\n  View: () => Pre.View('const js = true'),\n}"),
+      p([
+        'the source for this page is in the ',
+        a(
+          {
+            href: 'https://github.com/magic-modules/pre/tree/master/example',
+            target: '_blank',
+            rel: 'noopener',
+          },
+          'example directory',
+        ),
+      ]),
+    ]),
 }
