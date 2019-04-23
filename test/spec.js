5b4af099bd1d24361a44527d8a44b43c8fbec33b
Jascha <jascha@jaeh.at>
2019-04-24 00:53:16 +0200

    format tests

--- test/spec.js
+++ test/spec.js
@@ -1,6 +1,4 @@
 const { is } = require('@magic/test')
 const Pre = require('../src')
 
-module.exports = [
-  { fn: () => Pre, expect: is.object, info: 'expect Pre to be an object' },
-]
+module.exports = [{ fn: () => Pre, expect: is.object, info: 'expect Pre to be an object' }]
