87c7e9aa56dfa3f6e6c8d16a922279046e290005
Jascha <jascha@jaeh.at>
2019-04-30 01:12:46 +0200

    update spec

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,7 @@
 const { is } = require('@magic/test')
 const Pre = require('../src')
 
-module.exports = [{ fn: () => Pre, expect: is.object, info: 'expect Pre to be an object' }]
+module.exports = [
+  { fn: () => Pre, expect: is.function, info: 'expect Pre to be an object' },
+  { fn: () => Pre.View, expect: is.function, info: 'expect Pre to be an object' },
+]
