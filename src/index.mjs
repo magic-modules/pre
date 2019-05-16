8d57a055ffd3086e3442f27c5e62567b76e3b8ab
Jascha <jascha@jaeh.at>
2019-05-16 19:58:54 +0200

    use hyperapp v2

--- src/index.mjs
+++ src/index.mjs
@@ -1,189 +1,202 @@
 import path from 'path'
 
-export const View = (content, theme = '') => (state, actions) =>
-  CHECK_PROPS({ content, theme }, propTypes, 'Pre') &&
-  div({ class: `Pre ${theme || state.pre.theme}` }, [
+export const View = props => {
+  if (typeof props === 'string') {
+    props = {
+      content: props,
+      theme: 'light',
+      fixed: !!props.theme,
+    }
+  }
+
+  CHECK_PROPS(props, propTypes, 'Pre')
+
+  const { theme, content, fixed } = props
+
+  return div({ class: `Pre ${theme}` }, [
     div({ class: 'menu' }, [
-      !theme &&
-        button({ onclick: actions.pre.changeTheme }, state.pre.theme === 'dark' ? 'light' : 'dark'),
-      button({ onclick: () => actions.pre.clip(content) }, 'copy'),
+      !fixed &&
+        button({ onclick: [actions.pre.changeTheme] }, theme === 'dark' ? 'light' : 'dark'),
+      button({ onclick: [actions.pre.clip, content] }, 'copy'),
     ]),
     pre(LIB.Pre.format(content)),
   ])
+}
 
 export const state = {
   theme: 'light',
 }
 
 export const actions = {
-  changeTheme: () => state => ({
-    theme: state.theme === 'dark' ? 'light' : 'dark',
-  }),
-  clip: content => {
-    if (typeof document !== 'undefined' && typeof document.execCommand === 'function') {
-      const copy = document.createElement('textarea')
-      copy.id = 'copy'
-      copy.innerHTML = content
+  pre: {
+    changeTheme: state => ({
+      ...state,
+      theme: state.theme === 'dark' ? 'light' : 'dark',
+    }),
+    clip: content => {
+      if (typeof document !== 'undefined' && typeof document.execCommand === 'function') {
+        const copy = document.createElement('textarea')
+        copy.id = 'copy'
+        copy.innerHTML = content
 
-      document.body.appendChild(copy)
-      const child = document.getElementById('copy')
-      child.select()
+        document.body.appendChild(copy)
+        const child = document.getElementById('copy')
+        child.select()
 
-      document.execCommand('copy')
-      document.body.removeChild(child)
-    }
+        document.execCommand('copy')
+        document.body.removeChild(child)
+      }
+    },
   },
 }
 
 export const style = {
-  '.Pre': {
-    border: '1px solid #666',
-    borderRadius: '5px',
-    display: 'block',
-    fontFamily: 'monospace',
-    margin: '1em 0',
-    lineHeight: 1.3,
-    padding: '1em .5em 0 .2em',
-    position: 'relative',
-    whiteSpace: 'pre',
+  border: '1px solid #666',
+  borderRadius: '5px',
+  display: 'block',
+  fontFamily: 'monospace',
+  margin: '1em 0',
+  lineHeight: 1.3,
+  padding: '1em .5em 0 .2em',
+  position: 'relative',
+  whiteSpace: 'pre',
 
-    counterReset: 'line',
+  counterReset: 'line',
 
-    pre: {
-      overflowX: 'auto',
+  pre: {
+    overflowX: 'auto',
 
-      '> code': {
-        counterIncrement: 'line',
-        display: 'block',
+    '> code': {
+      counterIncrement: 'line',
+      display: 'block',
 
-        '&:before': {
-          content: 'counter(line)',
-          display: 'inline-block',
-          padding: '0 .5em 0 0',
-          textAlign: 'right',
-          userSelect: 'none',
-          width: '3ch',
-        },
+      '&:before': {
+        content: 'counter(line)',
+        display: 'inline-block',
+        padding: '0 .5em 0 0',
+        textAlign: 'right',
+        userSelect: 'none',
+        width: '3ch',
+      },
 
-        '&:last-child': {
-          padding: '0 0 1em',
-        },
+      '&:last-child': {
+        padding: '0 0 1em',
       },
     },
+  },
 
-    '.menu': {
-      position: 'absolute',
-      marginTop: '-2em',
-      right: 0,
+  '.menu': {
+    position: 'absolute',
+    marginTop: '-2em',
+    right: 0,
+  },
+
+  button: {
+    backgroundColor: '#666',
+    borderRadius: '3px',
+    color: '#fefefe',
+    margin: '0 .2em',
+    padding: '1px 3px',
+    border: '0 none',
+  },
+
+  '&.light': {
+    backgroundColor: '#eee',
+
+    '> code:before': {
+      color: '#666',
+    },
+    '.html': {
+      color: '#008800',
+    },
+    '.keyword': {
+      color: 'purple',
+    },
+    '.builtin': {
+      color: 'cadetblue',
+    },
+    '.string': {
+      color: '#d15100',
+    },
+    '.colon': {
+      color: '#016301',
+    },
+    '.boolean': {
+      color: 'blue',
+    },
+    '.actions': {
+      color: 'blueviolet',
+    },
+    '.state': {
+      color: 'cornflowerblue',
+    },
+    '.comment': {
+      color: '#555',
+      opacity: '0.8',
+      fontStyle: 'italic',
+    },
+    '.object': {
+      color: '#016301',
+    },
+    '.property': {
+      color: '#2a952a',
     },
 
-    button: {
-      backgroundColor: '#666',
-      borderRadius: '3px',
-      color: '#fefefe',
-      margin: '0 .2em',
-      padding: '1px 3px',
-      border: '0 none',
-    },
+    a: {
+      color: '#d15100',
 
-    '&.light': {
-      backgroundColor: '#eee',
-
-      '> code:before': {
-        color: '#666',
-      },
-      '.html': {
-        color: '#008800',
-      },
-      '.keyword': {
-        color: 'purple',
-      },
-      '.builtin': {
-        color: 'cadetblue',
-      },
-      '.string': {
-        color: '#d15100',
-      },
-      '.colon': {
-        color: '#016301',
-      },
-      '.boolean': {
-        color: 'blue',
-      },
-      '.actions': {
-        color: 'blueviolet',
-      },
-      '.state': {
-        color: 'cornflowerblue',
-      },
-      '.comment': {
-        color: '#555',
-        opacity: '0.8',
-        fontStyle: 'italic',
-      },
-      '.object': {
-        color: '#016301',
-      },
-      '.property': {
-        color: '#2a952a',
-      },
-
-      a: {
-        color: '#d15100',
-
-        '&:hover': {
-          color: 'turquoise',
-        },
-      },
-    },
-
-    '&.dark': {
-      backgroundColor: '#222',
-      color: '#eee',
-
-      '> code:before': {
-        color: '#666',
-      },
-      '.html': {
-        color: '#008800',
-      },
-      '.keyword': {
-        color: 'violet',
-      },
-      '.builtin': {
-        color: 'cadetblue',
-      },
-      '.string': {
-        color: '#dd8f00',
-      },
-      '.colon': {
-        color: '#8eef8e',
-      },
-      '.boolean': {
-        color: '#7979e4',
-      },
-      '.actions': {
-        color: 'blueviolet',
-      },
-      '.state': {
-        color: 'cornflowerblue',
-      },
-      '.comment': {
-        opacity: '0.7',
-        fontStyle: 'italic',
-      },
-      '.object': {
-        color: '#00ff00',
-      },
-      '.property': {
-        color: '#8eef8e',
-      },
-      a: {
+      '&:hover': {
         color: 'turquoise',
+      },
+    },
+  },
 
-        '&:hover': {
-          color: '#d15100',
-        },
+  '&.dark': {
+    backgroundColor: '#222',
+    color: '#eee',
+
+    '> code:before': {
+      color: '#666',
+    },
+    '.html': {
+      color: '#008800',
+    },
+    '.keyword': {
+      color: 'violet',
+    },
+    '.builtin': {
+      color: 'cadetblue',
+    },
+    '.string': {
+      color: '#dd8f00',
+    },
+    '.colon': {
+      color: '#8eef8e',
+    },
+    '.boolean': {
+      color: '#7979e4',
+    },
+    '.actions': {
+      color: 'blueviolet',
+    },
+    '.state': {
+      color: 'cornflowerblue',
+    },
+    '.comment': {
+      opacity: '0.7',
+      fontStyle: 'italic',
+    },
+    '.object': {
+      color: '#00ff00',
+    },
+    '.property': {
+      color: '#8eef8e',
+    },
+    a: {
+      color: 'turquoise',
+
+      '&:hover': {
+        color: '#d15100',
       },
     },
   },
