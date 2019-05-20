722011e783b8315d5c5c310c9a7c67a620c0aafe
Jascha <jascha@jaeh.at>
2019-05-20 08:03:45 +0200

    export pre plus lib... must be simpler in a future version

--- example/assets/index.mjs
+++ example/assets/index.mjs
@@ -1,1 +1,12 @@
-export * as Pre from '../../src/index.mjs'
+import path from 'path'
+
+import * as P from '../../src/index.mjs'
+
+const dirName = new URL(import.meta.url).pathname
+
+const lib = path.resolve(path.join(dirName, '..', '..', '..', 'src', 'lib', 'index.mjs'))
+
+export const Pre = {
+  ...P,
+  lib,
+}
